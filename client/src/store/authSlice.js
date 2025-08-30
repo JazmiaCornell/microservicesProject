d2e9c01ea429d3c33c52b86fb9d91e6f63b7a3e8
Jazmia Cornell <jazmiadc@gmail.com>
2025-08-30 13:48:48 -0400

    Fix jwtDecode import

--- client/src/store/authSlice.js
+++ client/src/store/authSlice.js
@@ -1,6 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import jwtDecode from "jwt-decode";
+// import jwtDecode from "jwt-decode";
+
+const jwtDecode = require("jwt-decode").default || require("jwt-decode");
 
 // Citation Scope: Implementation axios and redux for user authentication/create sessions
 // Date: 05/04/2025
